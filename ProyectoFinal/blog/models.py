da5ac271dfb12ccce61694432eed02a509903fd9
Matias Eguez <matias.eguez@warnermedia.com>
2022-01-20 15:22:55 -0300

    Models created

--- ProyectoFinal/blog/models.py
+++ ProyectoFinal/blog/models.py
@@ -9,16 +9,34 @@ STATUS = (
 )
 
 class Post(models.Model):
-    titulo = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    autor = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
-    actualizado = models.DateTimeField(auto_now= True)
-    contenido = models.TextField()
-    creado = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    thumbnail = models.ImageField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    #author = models.ForeignKey()
+    def __str__(self):
+        return self.title
 
-    class Meta:
-        ordering = ['-creado']
+class Comment(models.Model):
+    # user = models.ForeignKey()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
 
     def __str__(self):
-        return self.titulo
+        return self.user.username
+
+class PostView (models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+class Likes(models.Model):
+    # user = models.ForeignKey()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+    
